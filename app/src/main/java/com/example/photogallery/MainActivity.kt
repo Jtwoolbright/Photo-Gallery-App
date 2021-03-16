0613282433431c4c523deefc5824f2feec2b012d
jtwoolbright <woolbright.josh.t@gmail.com>
2021-03-16 14:06:07 -0700

    initial commit

---
+++ app/src/main/java/com/example/photogallery/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.photogallery
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val isFragmentContainerEmpty = savedInstanceState == null
+        if (isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, PhotoGalleryFragment.newInstance())
+                .commit()
+        }
+    }
+
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, MainActivity::class.java)
+        }
+    }
+}
